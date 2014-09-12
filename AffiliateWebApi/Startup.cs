f908d098f1443b40a9b3e1f046caf657abaf7962
rankizi <rankizi@gmail.com>
2014-09-12 18:56:43 +0300

    Initial Affiliate Site

---
+++ AffiliateWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AffiliateWebApi.Startup))]
+namespace AffiliateWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
